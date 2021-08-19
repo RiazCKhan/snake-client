2a5cf97d1829381215cf41d750d307cf9eaad3df
RiazK <RiazCKhan@gmail.com>
2021-08-19 17:36:22 -0400

    Moved handlerUserInput Scope, added connection

--- input.js
+++ input.js
@@ -1,19 +1,38 @@
+const { connect } = require("./client");
+
+let connection;
+
 // Setup User Interface 
 // Specifically, so that we can handle user input via stdin
-const setupInput = function() {
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
   stdin.resume();
   // Added Handling CTRL + C User Input to exit game.
-  const handleUserInput = stdin.on('data', (key) => {
-    if (key === '\u0003') {
-      process.exit();
-    }
-  })
+  stdin.on('data', handlerUserInput)
   return stdin;
 }
 
+const handleUserInput = (key) => {
+  if (key === '\u0003') {
+    process.exit();
+  }
+  if (key === '\u0077') {
+    connection.write("Move: up");
+  }
+  if (key === '\u0061') {
+    connection.write("Move: left");
+  }
+  if (key === '\u0073') {
+    connection.write("Move: down");
+  }
+  if (key === '\u0064') {
+    connection.write("Move: right");
+  }
+}
+
 module.exports = {
-  setupInput
+  setupInput,
 }
