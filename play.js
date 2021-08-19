ee5355d2092cf0b85000eb4f0db38b71beb41558
RiazK <RiazCKhan@gmail.com>
2021-08-19 15:36:25 -0400

    Added require setupInput

--- play.js
+++ play.js
@@ -2,24 +2,10 @@ const net = require('net');
 
 // play.js
 const { connect } = require('./client');
+const { setupInput } = require('./input');
+
+
 console.log('Connecting ...');
 connect();
 
-// Setup User Interface 
-// Specifically, so that we can handle user input via stdin
- const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-
-
-  const handlerUserInput = stdin.on('data', (key) => {
-    if (key === '\u0003') {
-      process.exit();
-    }
-  })
-
-
-  return stdin;
-}
+setupInput();
