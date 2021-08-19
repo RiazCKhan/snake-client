a9be70583f0c656d9a06201946a772b4dd6e125d
RiazK <RiazCKhan@gmail.com>
2021-08-19 15:01:56 -0400

    Manual Tested Movement via Hard Code

--- client.js
+++ client.js
@@ -2,23 +2,31 @@ const net = require('net');
 
 // Establishes connection with the game server
 const connect = function() {
-  // Connection print
-  console.log("Successfully Connected to Game Server")
-
+  
   const conn = net.createConnection({ 
     host: 'localhost',
     port: 50541,    
   });
-
+  
   // Event Handler to handle incoming data from server
   conn.on('data',(data) => {
-    console.log(data)
+    console.log('Almighty Server: ', data)
   })
-
-  // Sending a Name + Name Command
+  
+  // Connection Print + Sending a Name + Name Command
   conn.on('connect', () => {
+    console.log("Successfully Connected to The Almighty Server")
     conn.write('Name: RCK');
   });
+
+  // HARD CODE MOVEMENT EXPIREMENTS
+/*   conn.on('connect', () => {
+    conn.write("Move: up")
+
+      setInterval(() => {
+        conn.write("Move: left");
+      }, 100);
+  }) */
 
   // interpret incoming data as text
   conn.setEncoding('utf8'); 
@@ -29,3 +37,11 @@ const connect = function() {
 module.exports = {
   connect
 }
+
+
+/*
+ server.on('connection', (client) => {
+  console.log('New client connected!');
+  client.write('Hello there!');
+});
+ */
