910e1d7f6d9134020d8a5c5dee999454e8bbc82f
RiazK <RiazCKhan@gmail.com>
2021-08-20 14:53:33 -0400

    Updated with movement and message objects

--- constants.js
+++ constants.js
@@ -1,7 +1,23 @@
 const IP = "localhost";
 const PORT = 50541;
 
+const movement = {
+  '\u0077': "Move: up", // w
+  '\u0061': "Move: left", // a
+  '\u0073': "Move: down", // s
+  '\u0064': "Move: right" // d 
+}
+
+const messages = {
+  '\u007A': "Say: GLHF", // z
+  '\u0078': "Say: GG", // x
+  '\u0063': "Say: EZ" // c 
+}
+
+
 module.exports = {
   IP,
-  PORT
+  PORT,
+  movement,
+  messages
 };
